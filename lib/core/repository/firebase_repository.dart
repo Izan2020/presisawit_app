3ef137f7d66f4297ef0dd16d7db3bc2b432878bf
Glantrox <59131023+Izan2020@users.noreply.github.com>
2023-08-04 20:15:53 +0700

    deleted: unused files

--- lib/core/repository/firebase_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:presisawit_app/core/classes/models/company_response.dart';
-import 'package:presisawit_app/core/classes/logic/data_response.dart';
-
-abstract class FirebaseRepository {
-  Future<DataState<CompanyValidate?>> getCompanyData(String companyId);
-}
